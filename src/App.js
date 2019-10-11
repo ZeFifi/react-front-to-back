a1491b882d18c7be3ff2f783ec2fd29b9b185799
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2019-10-11 14:50:27 +0200

    HTTP Requests and updating state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import axios from "axios";
 import Navbar from "./components/Layout/Navbar";
 import Users from "./components/Users/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar icon="fab fa-github" />
-        <div className="container">
-            <Users />
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            loading: false
+        }
+    }
+
+    async componentDidMount() {
+        this.setState({ loading: true});
+        const res = await axios.get("https://api.github.com/users");
+        this.setState({users: res.data, loading: true})
+    }
+
+    render() {
+        const { loading, users } = this.state;
+        return (
+        <div className="App">
+          <Navbar icon="fab fa-github" />
+            <div className="container">
+                <Users loading={loading} users={users} />
+            </div>
         </div>
-    </div>
-  );
+      );
+    }
+
 }
 
 export default App;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,32 +2,6 @@ import React, {Component} from 'react';
 import UserItem from "./UserItem";
 
 class Users extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [
-            {
-                id: "1",
-                login: "mojombo",
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo",
-            },
-            {
-                id: "5550850",
-                login: "bradtraversy",
-                avatar_url: "https://avatars2.githubusercontent.com/u/5550850?v=4",
-                html_url: "https://github.com/bradtraversy",
-            },
-            {
-                id: "8557435",
-                login: "ZeFifi",
-                avatar_url: "https://avatars1.githubusercontent.com/u/8557435?v=4",
-                html_url: "https://github.com/ZeFifi",
-            },
-
-        ]}
-    }
-
     render() {
         const userStyle = {
             display: "grid",
@@ -37,7 +11,7 @@ class Users extends Component {
 
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
             </div>
