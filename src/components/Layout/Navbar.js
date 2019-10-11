acbda15e5f018b75f7735a3679e9f327c08fa917
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2019-10-11 14:20:04 +0200

    Stateless Functional Components

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,26 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 
-class Navbar extends Component {
-    render() {
+// Destructuring props on the line after instead of doing const { icon, title } = this.props;
+const Navbar = ({ icon, title }) => {
         return (
             <nav className="navbar bg-primary">
                 <h1>
-                    <i className={this.props.icon} />&nbsp; {this.props.title}
+                    <i className={icon} />&nbsp; {title}
                 </h1>
             </nav>
         );
-    }
+};
 
-    static defaultProps = {
-        title: "Github Finder",
-        icon: "fab fa-github"
-    };
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired,
-    }
-}
+Navbar.defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+};
+
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -1,9 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
-
+// Destructuring props on the line after, instead of doing const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
         return (
             <div className="card text-center">
                 <img src={avatar_url}
@@ -14,7 +13,13 @@ class UserItem extends Component {
                 <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a></div>
             </div>
         );
-    }
-}
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+    login: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired,
+};
 
 export default UserItem;
