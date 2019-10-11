84f3bb1297919166c74e6d70cd950ad06dc52aff
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2019-10-11 11:29:27 +0200

    Getting started with component state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/layout/Navbar";
+import Navbar from "./components/Layout/Navbar";
+import UserItem from "./components/Users/UserItem";
 
 function App() {
   return (
     <div className="App">
       <Navbar icon="fab fa-github" />
+      <UserItem />
     </div>
   );
 }
